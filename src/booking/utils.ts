655bc01a24127c7c558a8cdff71165ae059905b7
Mateo Galić <mateogalic112@gmail.com>
2022-12-10 23:53:56 +0100

    Format booking utils

--- src/booking/utils.ts
+++ src/booking/utils.ts
@@ -1,8 +1,18 @@
 import { Prisma } from '@prisma/client';
-import { differenceInCalendarDays, eachDayOfInterval, isEqual, sub } from 'date-fns';
-import { BookingCostInputs, FutureBookingsForProperty } from './booking.interface';
+import {
+  differenceInCalendarDays,
+  eachDayOfInterval,
+  isEqual,
+  sub,
+} from 'date-fns';
+import {
+  BookingCostInputs,
+  FutureBookingsForProperty,
+} from './booking.interface';
 
-export const getBookedDays = (futureBookings: FutureBookingsForProperty[] | undefined) => {
+export const getBookedDays = (
+  futureBookings: FutureBookingsForProperty[] | undefined,
+) => {
   if (!futureBookings) return [];
 
   const bookedDays: Date[] = futureBookings
@@ -18,23 +28,34 @@ export const getBookedDays = (futureBookings: FutureBookingsForProperty[] | unde
   return bookedDays;
 };
 
-export const checkBookedInterval = (startDate: Date, endDate: Date, bookedDays: Date[]) => {
+export const checkBookedInterval = (
+  startDate: Date,
+  endDate: Date,
+  bookedDays: Date[],
+) => {
   const intervalDays = eachDayOfInterval({
     start: startDate,
-    end: sub(endDate, { days: 1 }),
+    end: endDate,
   });
 
-  const isInvalidBooking = intervalDays.some((day) => bookedDays.find((bookedDay) => isEqual(bookedDay, day)));
+  const isInvalidBooking = intervalDays.some((day) =>
+    bookedDays.find((bookedDay) => isEqual(bookedDay, day)),
+  );
   return !isInvalidBooking;
 };
 
-export const calculateBookingCost = (bookingData: BookingCostInputs): Prisma.Decimal => {
-  const { startDate, endDate, adultsCount, childrenCount, pricePerNight } = bookingData;
+export const calculateBookingCost = (
+  bookingData: BookingCostInputs,
+): Prisma.Decimal => {
+  const { startDate, endDate, adultsCount, childrenCount, pricePerNight } =
+    bookingData;
   const totalNights = differenceInCalendarDays(endDate, startDate);
 
-  const totalAdultsPrice = parseFloat(pricePerNight.toString()) * totalNights * adultsCount;
+  const totalAdultsPrice =
+    parseFloat(pricePerNight.toString()) * totalNights * adultsCount;
 
-  const totalChildrenPrice = parseFloat(pricePerNight.toFixed(2)) * 0.8 * totalNights * childrenCount;
+  const totalChildrenPrice =
+    parseFloat(pricePerNight.toFixed(2)) * 0.8 * totalNights * childrenCount;
 
   const preliminaryCost = totalAdultsPrice + totalChildrenPrice;
   return new Prisma.Decimal(parseFloat(preliminaryCost.toFixed(2)));
