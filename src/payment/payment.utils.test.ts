ef99d8656fd30743c13d821b323e9e0b939b83e9
Mateo Galić <mateogalic112@gmail.com>
2023-01-11 20:54:59 +0100

    Update booking refund calculation test

--- src/payment/payment.utils.test.ts
+++ src/payment/payment.utils.test.ts
@@ -4,16 +4,9 @@ import { calculateBookingRefund } from './payment.utils';
 describe('Calculate booking refund correctly', () => {
   const BOOKING_PRICE = 1000.0;
   it('should refund 80% of booking price if booking is cancelled at least 30 days before booking start', () => {
-    const FORTY_DAYS_BEFORE_BOOKING_START = new Date(addDays(new Date(), 40));
-    const bookingRefundFortyDaysBeforeBooking = calculateBookingRefund(
-      FORTY_DAYS_BEFORE_BOOKING_START,
-      BOOKING_PRICE,
-    );
-    expect(bookingRefundFortyDaysBeforeBooking).toBe(800.0);
-
-    const THORTY_DAYS_BEFORE_BOOKING_START = new Date(addDays(new Date(), 30));
+    const THIRTY_DAYS_BEFORE_BOOKING_START = new Date(addDays(new Date(), 30));
     const bookingRefundThirtyDaysBeforeBooking = calculateBookingRefund(
-      THORTY_DAYS_BEFORE_BOOKING_START,
+      THIRTY_DAYS_BEFORE_BOOKING_START,
       BOOKING_PRICE,
     );
     expect(bookingRefundThirtyDaysBeforeBooking).toBe(800.0);
